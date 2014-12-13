feb5d29bac4c534d8f022e1ba617fb9faf4a1201
Oleg <diolen3005@gmail.com>
2014-12-13 18:26:30 -0300

    Doctrine2 buch application

---
+++ config/doctrine.php
@@ -0,0 +1,9 @@
+<?php
+$paths = array(__DIR__ . "/../src/Entity/");
+$isDevMode = true;
+$dbParams = array(
+    'driver'   => 'pdo_mysql',
+    'user'     => 'dev',
+    'password' => 'dev',
+    'dbname'   => 'doctrine2buch-application',
+);
